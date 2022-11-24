a347df720cc6b3fb40d4a976e134b55e9c5ac2e1
prog-nori <norinorimcz@gmail.com>
2022-11-24 21:14:00 +0900

    the first commit

---
+++ command_parser.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+#  -*- coding: utf-8 -*-
+
+class CommandParser:
+    def __init__(self, query):
+        self.query = query
+        pass
+
+    def want_play(self):
+        pass
+
+    def want_skip(self):
+        pass
+
+    def want_show_queue(self):
+        pass
+
+    def want_stop(self):
+        pass
+
+    def want_resume(self):
+        pass
+
+    def want_disconnect(self):
+        pass
---
+++ id.py
@@ -0,0 +1,3 @@
+application_id = 1040601936396812389
+public_key = '0368124ffce9f9896abe4f2af09b1ca789fa456909c0f8d1eca7d31f6f163edd'
+token = 'MTA0MDYwMTkzNjM5NjgxMjM4OQ.Gg2bYN.pC56YQvUvq5yACfNxjCBI64pMi2yzw-i6FjkhQ'
